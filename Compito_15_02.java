a75b5a031a8b70690006d4fd05558fa4ad165047
lai2-01 <lai2-01@itismeucci.com>
2022-02-15 11:40:49 +0100

    primo commit

---
+++ Compito_15_02.java
@@ -0,0 +1,10 @@
+
+package compito_15_02;
+
+public class Compito_15_02 {
+    public static void main(String args[]){
+        for (int i = 0; i < 9; i++){
+            Numero n = new Numero (5);
+        }
+    }
+}
---
+++ Numero.java
@@ -0,0 +1,19 @@
+
+package compito_15_02;
+
+public class Numero{
+    private int n;
+
+    public Numero(int n){
+        this.n = n;
+    }
+
+    public int getNumero(){
+        return n;
+    }
+
+    public void setNumero(int n){
+        this.n=n;
+    }
+    
+}
